51d82ecf619ffd792f209437ef60f7205b5a7305
Donald Mennerich <don.mennerich@nyu.edu>
2020-09-22 14:20:16 -0400

    updating script

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"github.com/nyudlts/go-aspace/lib"
+	"os"
 )
 
 var aspace = lib.Client
+
+var t = newTrue()
 
 func checkPanic(err error) {
 	if err != nil {
@@ -19,32 +24,50 @@ func newTrue() *bool {
 }
 
 func main() {
+	//get the repository id from the command line
+	repPntr := flag.Int("repository", 0, "the repository")
+	flag.Parse()
 
-	repositoryId := 2
-	daos, err := aspace.GetDigitalObjectsByRepositoryId(repositoryId)
+	logFile, err := os.Create(fmt.Sprintf("logfile-%d.txt", *repPntr))
+	checkPanic(err)
+	defer logFile.Close()
+	logWriter := bufio.NewWriter(logFile)
+
+	daos, err := aspace.GetDigitalObjectsByRepositoryId(*repPntr)
 	checkPanic(err)
 
-	for daoId := range daos {
-		fmt.Println(repositoryId, daoId)
+	for _, daoId := range daos {
 
-		dao, err := aspace.GetDigitalObject(repositoryId, daoId)
+		dao, err := aspace.GetDigitalObject(*repPntr, daoId)
 		checkPanic(err)
 
+		//check if there are file versions
 		if len(dao.FileVersions) > 0 {
+			var update = false
+			//check fileversion subrecords for ones where publish value is nil
 			for _, fv := range dao.FileVersions {
 				if fv.Publish == nil {
+					//If the parent DAO's publish field is set to True, update
 					if dao.Publish == true {
+						update = true
 						fv.Publish = newTrue()
 					}
 				}
 			}
 
+			if update == true {
+				fmt.Println("Updating", dao.URI)
+				updateResponse, err := aspace.PostDigitalObject(*repPntr, daoId, dao)
+				if err != nil {
+					logWriter.WriteString(dao.URI + "\t" + err.Error())
+					logWriter.Flush()
+				}
+				fmt.Println(updateResponse)
+			}
 		}
 
-		//check that fileversions are updated
-
-		// update dao in aspace
-
 	}
+
+	logWriter.Flush()
 
 }
